5caceff952a77705f21c45ef3e10c27079e7dbfb
Sergey <artamonov4work@yandex.ru>
2026-07-29 00:34:56 +0300

    feat: Добавлен usersSlice c тестами и подключен к стору

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 // Импортируй свои slice'ы здесь по мере их создания:
 // import skillsReducer from '@/entities/skill/model/skillsSlice'
 // import authReducer from '@/features/auth/model/authSlice'
+import usersReducer from '@/entities/user/model/usersSlice'
 
 export const store = configureStore({
   reducer: {
     // skills: skillsReducer,
     // auth: authReducer,
+    users: usersReducer,
   },
 })
 
