a49c3ee279c2307086bbeb43fd20156ff009551a
Svetlana <florens@mail.ru>
2026-07-30 18:06:51 +0300

    refactor: убрать работу с данными в SearchInput

--- src/shared/ui/SearchInput/SearchInput.test.tsx
+++ src/shared/ui/SearchInput/SearchInput.test.tsx
@@ -1,30 +1,38 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { SearchInput } from './SearchInput';
 
 describe('SearchInput', () => {
   it('отображает placeholder', () => {
-    render(<SearchInput />);
+    render(
+  <SearchInput value='' placeholder='Искать навык' onChange={() => {}}/>
+);
 
     expect(
       screen.getByPlaceholderText('Искать навык')
     ).toBeInTheDocument();
   });
 
-  it('обновляет значение при вводе текста', () => {
-    render(<SearchInput />);
+  it('вызывает onChange при вводе текста', () => {
+  const handleChange = vi.fn();
 
-    const input = screen.getByRole('textbox');
+  render(
+    <SearchInput value='' placeholder='Искать навык' onChange={handleChange}/>
+  );
 
-    fireEvent.change(input, {
-      target: { value: 'React' },
-    });
-
-    expect(screen.getByDisplayValue('React')).toBeInTheDocument();
+  fireEvent.change(screen.getByRole('textbox'), {
+    target: { value: 'React' },
   });
 
+  expect(handleChange).toHaveBeenCalledTimes(1);
+});
+
   it('отображает иконку поиска', () => {
-    const { container } = render(<SearchInput />);
+    const { container } = render(<SearchInput
+    value=""
+    onChange={() => {}}
+    placeholder="Искать навык"
+  />);
 
     expect(container.querySelector('svg')).toBeInTheDocument();
   });
--- src/shared/ui/SearchInput/SearchInput.tsx
+++ src/shared/ui/SearchInput/SearchInput.tsx
@@ -1,25 +1,13 @@
-import { ChangeEvent, useState, useEffect } from 'react';
-import { Input } from "../Input";
+import { Input, type InputProps } from "../Input";
 import { LoupeIcon } from "./LoupeIcon";
-import { useDebounce } from '../../hooks/useDebounce';
 
-export function SearchInput() {
-  const [value, setValue] = useState('');
-  const debouncedValue = useDebounce(value, 300);
+export type SearchInputProps = Omit<InputProps, "leftIcon">;
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-  }
-
-  useEffect(() => {
-    console.log(debouncedValue)
-  }, [debouncedValue])
+export function SearchInput(props: SearchInputProps) {
 
   return (
     <Input
-    value={value}
-    placeholder="Искать навык"
-    onChange={handleChange}
+    {...props}
     leftIcon={<LoupeIcon />}
     />
   )
