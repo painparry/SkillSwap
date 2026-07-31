ef4e4c98e482a17578716efea3316ed00ac856f0
Dmitry Fedotochkin <dkrouli@yandex.ru>
2026-07-31 18:38:35 +0300

    Создан загрузчик аватара

---
+++ src/shared/ui/AvatarUpload/AvatarUpload.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import addIcon from './add-icon.svg'
+import styles from './AvatarUpload.module.css'
+
+export const AvatarUpload = () => {
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null)
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (file) {
+      if (previewUrl) {
+        URL.revokeObjectURL(previewUrl)
+      }
+      const url = URL.createObjectURL(file)
+      setPreviewUrl(url)
+    }
+  }
+
+  return (
+    <label className={styles.avatarContainer}>
+      {!previewUrl && (
+        <img src={addIcon} alt="Добавить аватар" className={styles.iconPlaceholder} />
+      )}
+
+      {previewUrl && <img src={previewUrl} alt="Avatar" className={styles.imagePreview} />}
+      <input
+        type="file"
+        accept="image/jpg, image/png, image/jpeg"
+        className={styles.fileInput}
+        onChange={handleFileChange}
+      />
+    </label>
+  )
+}
