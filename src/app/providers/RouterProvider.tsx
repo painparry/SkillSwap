af2e76f78ce2d533738a9b412543cc76c27ae7c1
Timur Sanatov <neizpro@gmail.com>
2026-08-09 19:23:04 +0300

    feat: Сделал лэйауты для проекта

--- src/app/providers/RouterProvider.tsx
+++ src/app/providers/RouterProvider.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 import { ROUTES } from '@/shared/lib/constants'
-import { Header } from '@/widgets/Header'
-import { Footer } from '@/widgets/Footer/'
 import styles from './RouterProvider.module.css'
+import { AuthLayout, HeaderOnlyLayout, MainLayout } from '@/app/layouts'
 
 // Lazy-загрузка страниц — каждая страница грузится только при переходе на неё
+
 const CatalogPage = lazy(() => import('@/pages/CatalogPage'))
 const SkillPage = lazy(() => import('@/pages/SkillPage'))
 const ProfilePage = lazy(() => import('@/pages/ProfilePage'))
@@ -16,37 +16,37 @@ const NotFoundPage = lazy(() => import('@/pages/NotFoundPage'))
 const ServerErrorPage = lazy(() => import('@/pages/ServerErrorPage'))
 const RegistrationStep1Page = lazy(() => import('@/pages/RegistrationStep1Page'))
 
-function isAuthPage(pathname: string) {
-  return (
-    pathname === ROUTES.LOGIN ||
-    pathname === ROUTES.REGISTER ||
-    pathname.startsWith('/registration')
-  )
-}
-
 function AppLayout() {
-  const { pathname } = useLocation()
-
   return (
     <div className={styles.app}>
-      {!isAuthPage(pathname) && <Header />}
       <Routes>
-        <Route path={ROUTES.HOME} element={<CatalogPage />} />
-        <Route path={ROUTES.SKILL} element={<SkillPage />} />
-        <Route path={ROUTES.FAVORITES} element={<FavoritesPage />} />
-        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-        <Route path={ROUTES.REGISTER} element={<LoginPage />} />
-        <Route path={ROUTES.REGISTRATION_STEP_1} element={<RegistrationStep1Page />} />
+        <Route element={<HeaderOnlyLayout />}>
+          <Route path={ROUTES.HOME} element={<CatalogPage />} />
+        </Route>
+
+        <Route element={<MainLayout />}>
+          <Route path={ROUTES.SKILL} element={<SkillPage />} />
+          <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
+          <Route path={ROUTES.CREATE} element={<CreateSkillPage />} />
+          <Route path={ROUTES.FAVORITES} element={<FavoritesPage />} />
+          <Route path={ROUTES.NOT_FOUND_ERROR} element={<NotFoundPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path={ROUTES.SERVER_ERROR} element={<ServerErrorPage />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+          <Route
+            path={ROUTES.REGISTER}
+            element={<Navigate to={ROUTES.REGISTRATION_STEP_1} replace />}
+          />
+          <Route path={ROUTES.REGISTRATION_STEP_1} element={<RegistrationStep1Page />} />
+          <Route path={ROUTES.REGISTRATION_STEP_2} element={<RegistrationStep1Page />} /> {/* TODO: заменить на RegistrationStep2Page */}
+          <Route path={ROUTES.REGISTRATION_STEP_3} element={<RegistrationStep1Page />} />{/* TODO: заменить на RegistrationStep3Page */}
+        </Route>
 
         {/* Защищённые маршруты — добавь PrivateRoute обёртку */}
-        <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
-        <Route path={ROUTES.CREATE} element={<CreateSkillPage />} />
-
-        <Route path={ROUTES.NOT_FOUND_ERROR} element={<NotFoundPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path={ROUTES.SERVER_ERROR} element={<ServerErrorPage />} />
       </Routes>
-      <Footer />
     </div>
   )
 }
