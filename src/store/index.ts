0934146036a6941626a0ba8659fdc4f8d9f68638
slava <vaceslavkozakevic84@gmail.com>
2026-08-09 12:49:52 +0300

    разрешение конфликта в сторе

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '@/features/auth/model/authSlice';
 import usersReducer from '@/entities/user/model/usersSlice';
-import requestsReducer from '@/entities/request/model/requestsSlice';
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,   
-    users: usersReducer,
-    requests: requestsReducer,
-=======
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '@/features/auth/model/authSlice'
-import usersReducer from '@/entities/user/model/usersSlice'
-import skillsReducer from '@/entities/skill/model/skillsSlice'
+import skillsReducer from '@/entities/skill/model/skillsSlice';   
+import requestsReducer from '@/entities/request/model/requestsSlice'; 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     users: usersReducer,
-    skills: skillsReducer,
->>>>>>> develop
+    skills: skillsReducer,    
+    requests: requestsReducer, 
   },
-})
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
