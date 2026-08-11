1178350a7ca5340871de0a143da436de876de98a
KonstantinZin <thekonstanta228@mail.ru>
2026-08-11 17:14:55 +0400

    feat: switch between auth and unauth headers based on isAuth

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
-import { Footer } from '@/widgets/Footer'
+import { useAppSelector } from '@/store/hooks'
+import { HeaderUI } from '@/shared/ui/Header/Header'
 import { Header } from '@/widgets/Header'
+import { Footer } from '@/widgets/Footer'
 import { Outlet } from 'react-router-dom'
 
 export const MainLayout = () => {
+  const isAuth = useAppSelector((state) => state.auth.isAuth)
+
   return (
     <>
-      <Header />
+      {isAuth ? <Header /> : <HeaderUI />}
       <Outlet />
       <Footer />
     </>
