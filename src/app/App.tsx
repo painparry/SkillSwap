dc66c939452fabebb0296f522914137271d0bf01
KonstantinZin <thekonstanta228@mail.ru>
2026-08-12 22:03:48 +0400

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,6 @@ import { AppRouter } from './providers/RouterProvider'
 import { BrowserRouter } from 'react-router-dom'
 import './styles/global.css'
 import '../assets/styles/fonts.css'
-import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
